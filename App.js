7d2e5e4ac37458c880f6190b8722c7ee32f250f1
SamuelHGerber <henrylightfoot@gmail.com>
2021-10-18 17:15:58 -0700

    basic exploration of components with image issue

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView>
       <Text>Now we're talking!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Image source={require("./reddit-avatar.png")} style={{ width: 100 }} />
+      <Image
+        source={{ uri: "https://reactjs.org/logo-og.png" }}
+        style={{ width: 400, height: 400 }}
+      />
+      <TextInput
+        style={{ height: 60 }}
+        placeholder="Now's your turn to respond!"
+      />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
