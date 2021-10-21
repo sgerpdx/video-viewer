f332db5a5153bd73071d730d5fd3db6a576a2752
SamuelHGerber <henrylightfoot@gmail.com>
2021-10-21 06:45:16 -0700

    header formatting and button added

--- App.js
+++ App.js
@@ -7,57 +7,72 @@ import {
   ScrollView,
   Image,
   TextInput,
+  Button,
 } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={[styles.container, { flexDirection: "row", flexWrap: "wrap" }]}
-    >
-      <View style={styles.colorSquare}>
-        <Text>1</Text>
+    <View style={styles.outerBox}>
+      <View style={styles.titleRow}>
+        <Text style={styles.generalText}>Video Viewer</Text>
+        <View style={styles.iconRow}>
+          {" "}
+          <Image
+            source={{ uri: "https://reactjs.org/logo-og.png" }}
+            style={{ width: 24, height: 24 }}
+          ></Image>
+          <Image
+            source={{ uri: "https://reactjs.org/logo-og.png" }}
+            style={{ width: 24, height: 24 }}
+          ></Image>
+          <Image
+            source={{ uri: "https://reactjs.org/logo-og.png" }}
+            style={{ width: 24, height: 24 }}
+          ></Image>
+        </View>
       </View>
-      <View style={styles.colorSquare}>
-        <Text>2</Text>
-      </View>
-      <View style={styles.colorSquare}>
-        <Text>3</Text>
-      </View>
-      <View style={styles.colorSquare}>
-        <Text>4</Text>
-      </View>
-      <View style={styles.colorSquare}>
-        <Text>5</Text>
-      </View>
-      <View style={styles.colorSquare}>
-        <Text>6</Text>
-      </View>
-      <View style={styles.colorSquare}>
-        <Text>7</Text>
-      </View>
-      <View style={styles.colorSquare}>
-        <Text>8</Text>
-      </View>
-      <View style={styles.colorSquare}>
-        <Text>9</Text>
-      </View>
-      {/* <View>
-        <button>push me</button>
-      </View> */}
-      {/* <Text>d i v i d e r</Text>
-      <Image
-        source={{ uri: "https://reactjs.org/logo-og.png" }}
-        style={{ width: 400, height: 400 }}
-      />
-      <TextInput
-        style={{ height: 60 }}
-        placeholder="Enter text here."
-      /> */}
+      <TextInput style={{ height: 60 }} placeholder="Select video source:" />
+      <ScrollView>
+        <Image
+          source={{ uri: "https://reactjs.org/logo-og.png" }}
+          style={{ width: 240, height: 240 }}
+        />
+        <Button title="Play/Pause" color="green" />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  outerBox: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "blue",
+    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 360,
+    height: 720,
+  },
+  titleRow: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: 320,
+    height: 120,
+  },
+  generalText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  iconRow: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    width: 120,
+  },
   container: {
     flex: 1,
     backgroundColor: "blue",
@@ -76,5 +91,14 @@ const styles = StyleSheet.create({
     borderColor: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  controlBox: {
+    flex: 1,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    width: 160,
+    height: 160,
   },
 });
