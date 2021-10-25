1c25270ca44fda1647d32a6ca2a32e5d9340a214
SamuelHGerber <henrylightfoot@gmail.com>
2021-10-25 06:26:09 -0700

    code cleanup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import {
   StyleSheet,
   Text,
@@ -21,7 +21,6 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-
       <Stack.Navigator>
         <Stack.Screen
           name="Main"
@@ -39,70 +38,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  outerBox: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "blue",
-    color: "white",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 420,
-    height: 720,
-  },
-  titleRow: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: 320,
-    height: 120,
-  },
-  generalText: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  iconRow: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    width: 120,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-    alignContent: "flex-start",
-    padding: 10,
-    width: 160,
-    height: 160,
-  },
-  colorSquare: {
-    backgroundColor: "yellow",
-    width: 40,
-    height: 40,
-    borderWidth: 4,
-    borderColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  controlBox: {
-    flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 10,
-    width: 160,
-    height: 160,
-  },
-  videoZone: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-});
--- src/components/VideoScreen.jsx
+++ src/components/VideoScreen.jsx
@@ -71,63 +71,6 @@ export default function VideoScreen() {
 }
 
 const styles = StyleSheet.create({
-  outerBox: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "blue",
-    color: "white",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 420,
-    height: 720,
-  },
-  titleRow: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: 320,
-    height: 120,
-  },
-  generalText: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  iconRow: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    width: 120,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-    alignContent: "flex-start",
-    padding: 10,
-    width: 160,
-    height: 160,
-  },
-  colorSquare: {
-    backgroundColor: "yellow",
-    width: 40,
-    height: 40,
-    borderWidth: 4,
-    borderColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  controlBox: {
-    flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 10,
-    width: 160,
-    height: 160,
-  },
   videoZone: {
     position: "absolute",
     top: 0,
