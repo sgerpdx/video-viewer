680058eec01244b3fdb5f306b2f4f377ed330e70
SamuelHGerber <henrylightfoot@gmail.com>
2021-11-02 09:01:00 -0700

    testing dependencies installed and code cleanup

--- src/containers/Main.test.jsx
+++ src/containers/Main.test.jsx
@@ -1,26 +1,10 @@
-import React from "react";
-
-// the following suggested by https://jestjs.io/docs/tutorial-react-native but npm fails:
-//import renderer from "react-test-renderer";
-
-import { render, screen, waitFor } from "@testing-library/react";
-// import { MemoryRouter, Route } from 'react-router-dom';
-// import userEvent from '@testing-library/user-event';
-// import { rest } from 'msw';
-// import { setupServer } from 'msw/node';
-
+import { render } from "@testing-library/react-native";
 // importing of component to test:
-import MainScreen from "./MainScreen";
+import { MainScreen } from "./MainScreen";
 
-describe("MainScreen video container component", () => {
-  it("displays a dropdown menu for source selection", async () => {
-    render(<MainScreen />);
+// this basic test based on docs at: https://callstack.github.io/react-native-testing-library/docs/api/
 
-    // const loading = await screen.getByText("Loading...");
-    // expect(loading).toMatchSnapshot();
-    const dropdownMenu = await screen.getByText("Select video source:");
-    expect(dropdownMenu).toMatchSnapshot();
-
-    // add a return waitFor once mock is set up
-  });
+test("should display selection text", () => {
+  const { getByText } = render(<MainScreen />);
+  const element = getByText("Select video source:");
 });
